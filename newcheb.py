fdd50e4d02bedd7495759971e01419129a1c0802
Irene Mizus <irene.mizus@gmail.com>
2021-04-26 23:58:22 +0300

    A propagation part of the propagation task for a system of harmonic oscillator is added. Not yet tested.

--- newcheb.py
+++ newcheb.py
@@ -205,6 +205,187 @@ def hamil(psi, v, akx2, np):
     return phi
 
 # ----------------------------------------------------------
+def fold(src):
+    """ Takes 2-D numpy array with even number of columns.
+        Creating array of twice the height and half width by copying
+        the right half of the input array under the left half """
+
+    init_height = src.shape[0]
+    init_width = src.shape[1]
+
+    assert init_width % 2 == 0
+    res_height = init_height * 2
+    res_width = init_width // 2
+
+    res = numpy.zeros((res_height, res_width))
+    res[:init_height,:res_width] = src[:init_height,:res_width]
+    res[init_height:res_height, :res_width] = src[:init_height, res_width:init_width]
+
+    return res
+
+
+def test_fold():
+    sss = numpy.array([[1, 2, 3, 4], [5, 6, 7, 123]])
+    assert (fold(sss) == numpy.array([[1, 2], [5, 6], [3, 4], [7, 123]])).all()
+
+
+test_fold()
+
+# ----------------------------------------------------------
+def reorder(nch):
+    """ Shuffles the order of points in the Chebyshev interpolation scheme
+        INPUT
+        nch   number of interpolation points (must be a power of 2)
+        OUTPUT
+        jj    integer vector of length nch containing order of interpolation points """
+
+    assert (nch & (nch - 1) == 0) and nch > 0  # nch is a positive power of two
+    folds_count = int(math.log2(nch))
+
+    jj = numpy.zeros((nch))
+    for i in range(nch):
+        jj[i] = i
+
+    jj = numpy.reshape(jj, (1,nch))
+    for i in range(folds_count):
+        jj = fold(jj)
+    numpy.reshape(jj, (nch))
+
+    res = []
+    for i in range(nch):
+        res.append(int(jj[i]))
+
+    return res
+
+
+def test_reorder():
+    jj1 = reorder(2)
+    assert (jj1 == numpy.array([0, 1])).all()
+    jj2 = reorder(8)
+    assert (jj2 == numpy.array([0, 4, 2, 6, 1, 5, 3, 7])).all()
+
+
+test_reorder()
+
+# ----------------------------------------------------------
+def func(z, t):
+    """ The function to be interpolated
+        INPUT
+        z     real coordinate parameter
+        t     real time parameter
+        OUTPUT
+        func  value of the function (complex)
+        func = f (z, t) """
+
+    return cmath.exp(-1j * z * t)
+
+# ----------------------------------------------------------
+def points(nch, t):
+    """ Calculation of interpolation points and divided difference coefficients
+        INPUT
+        nch   number of interpolation points (must be a power of 2 if reorder is necessary)
+        t     scaled time interval
+        OUTPUT
+        xp    real vector of length nch defining the positions of the interpolation points				^
+        dv    complex vector of length nch defining the divided differences """
+
+    # defining the order of the interpolation points
+    jj = reorder(nch)
+
+    # calculating of the Chebyshev interpolation points
+    xp = []
+    for i in range(nch):
+        phase = float(2 * jj[i] + 1) / float(2 * nch) * math.pi
+        xp.append(2.0 * math.cos(phase))
+
+    dv = []
+    # calculating the first two terms of the divided difference
+    dv.append(func(xp[0], t))
+    dv.append((func(xp[1], t) - dv[0]) / (xp[1] - xp[0]))
+
+    # recursion for the divided difference
+    for i in range(2, nch):
+        res = 1.0
+        sum = complex(0.0, 0.0)
+        for j in range(i - 1):
+            res *= (xp[i] - xp[j])
+            sum += dv[j + 1] * res
+        res *= (xp[i] - xp[i - 1])
+        dv.append((func(xp[i], t) - dv[0] - sum) / res)
+
+    return xp, dv
+
+#xp, dv = points(8, 0.1)
+#print(xp)
+#print(dv)
+
+# ----------------------------------------------------------
+def residum(psi, v, akx2, xp, np, emax):
+    """ Scaled and normalized mapping phi = ( O - xp I ) phi
+        INPUT
+        psi  complex vector of length np
+        v    potential energy of length np
+        xp   sampling interpolation point
+        np   number of grid points (must be a power of 2)
+        emax range of energy spectrum
+        OUTPUT
+        phi  complex vector of length np
+             the operator is normalized from -2 to 2 resulting in:
+             phi = (4.O / emax - 2I) phi - xp I phi	(emin = 0) """
+
+    hpsi = hamil(psi, v, akx2, np)
+
+    # changing the range from -2 to 2
+    phi = []
+    for i in range(np):
+        hpsi[i] = 2.0 * (2.0 * hpsi[i] / emax - psi[i])
+        phi[i].append(hpsi[i] - xp * psi[i])
+
+    return phi
+
+# ----------------------------------------------------------
+def prop(psi, phi, t, nch, np, v, akx2):
+    """ Propagation subroutine using Newton interpolation
+        P(O) psi = dv(1) psi + dv2 (O - x1 I) psi + dv3 (O - x2)(O - x1 I) psi + ...
+        INPUT
+        psi  complex vector of length np describing wavefunction
+             at the beginning of interval
+        phi  auxiliary vector used for recurrence
+        t    time interval
+        nch  order of interpolation polynomial (must be a power of 2 if
+             reorder is necessary)
+        np   number of grid points (must be a power of 2)
+        v    potential energy vector of length np
+        akx2 kinetic energy vector of length np
+        OUTPUT
+        psi  complex vector of length np
+             describing the propagated wavefunction
+             phi(t) = exp(-iHt) psi(0) """
+
+    # calculating the energy range of the Hamiltonian operator H
+    emax = v[0] + abs(akx2(int(np / 2))) + 2.0
+    t_sc = t * emax / 4.0
+    print("emax = %f", emax, "scaled time interval = %f", t_sc)
+
+    # interpolation points and divided difference coefficients
+    xp, dv = points(nch, t_sc)
+    phi[:] = psi[:]
+
+    # accumulating first term
+    psi = [el * dv[0] for el in psi]
+
+    # recurrence loop
+    for i in range(nch - 1):
+        # mapping by scaled operator of phi
+        phi = residum(phi, v, akx2, xp[i], np, emax)
+        # accumulation of Newtonian's interpolation
+        for j in range(np):
+            psi[j] += dv[i + 1] * phi[j]
+        psi = [el * cmath.exp(1j * 2.0 * t_sc) for el in psi]
+
+    return psi
+
+# ----------------------------------------------------------
 def main(argv):
     """ The main() function """
     # analyze cmdline:
@@ -310,6 +491,11 @@ must be positive. Exiting", sys.stderr)
     phi0 = hamil(psi0, v, akx2, np)
     cener0 = cprod(phi0, psi0, dx, np)
     print("Initial energy: %f" % abs(cener0))
+
+#    jj = reorder(nch)
+#    for i in range(nch):
+#        print(i, jj[i])
+
 
 
 
