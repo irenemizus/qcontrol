0de857e81aa9774f4d48ae7cf86ec37cb868e4eb
Irene Mizus <irene.mizus@gmail.com>
2022-09-14 12:55:11 +0300

    Bash batch file added -1.

--- run-batch.sh
+++ run-batch.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 #python newcheb.py --json-rep "input_report.json" --json-task "input_task_ut_ang_mom_H_2lvls.json" > out.txt 2>err &
+#
+#SBATCH --job-name=try500
+#SBATCH --output=er_%j.txt
+#
+#SBATCH --cpus-per-task=1
+#SBATCH --threads-per-core=1
+##SBATCH --shared
+#SBATCH -A all-account
+#SBATCH -p allq
+#SBATCH --mem=5000
+##SBATCH --time=12:00:00
 python math_base_tests.py
