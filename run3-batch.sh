7c238477594d235dcb2ad53c3cb11670c3004da4
Irene Mizus <irene.mizus@gmail.com>
2022-09-22 14:28:00 +0300

    The scripts are updated

--- run3-batch.sh
+++ run3-batch.sh
@@ -8,6 +8,6 @@
 ##SBATCH --shared
 #SBATCH -A ronnie-account
 #SBATCH -p ronnieq
-#SBATCH --mem=5000
+#SBATCH --mem=1000
 ##SBATCH --time=12:00:00
-python newcheb.py --json_rep "input_report.json" --json_task "batch_jsons/input_task_ut_ang_mom_H_run3_var$i.json" > batch_jsons_out/out3_$1.txt 2>batch_jsons_out/err3_$1 &
+python newcheb.py --json_rep "input_report.json" --json_task "batch_jsons/input_task_ut_ang_mom_H_run3_var$1.json" > batch_jsons_out/out3_$1.txt 2>batch_jsons_out/err3_$1
