38ad2528435d3dfb9cd6996c4e93c7bb7c9e47cf
Irene Mizus <irene.mizus@gmail.com>
2022-10-27 22:59:30 +0300

    The input and batch files are prepared for analyzing of the dynamics for 6 different calculation times on cluster.

---
+++ run-batch_td_2030fs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+#SBATCH --job-name=try500
+#SBATCH --output=er_%j.txt
+#
+#SBATCH --cpus-per-task=1
+#SBATCH --threads-per-core=1
+##SBATCH --shared
+#SBATCH -A ronnie-account
+#SBATCH -p ronnieq
+#SBATCH --mem=1000
+##SBATCH --time=12:00:00
+python newcheb.py --json_rep "input_report_test_dynamics_2030fs.json" --json_task "input_task_ut_ang_mom_H_2lvls_test_dynamics_2030fs.json" > batch_jsons_out/out_2030fs.txt 2>batch_jsons_out/err_2030fs
+
