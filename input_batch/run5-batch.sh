1a629b314b454a9d66c3bc244208d2621d9cf617
Irene Mizus <irene.mizus@gmail.com>
2022-10-28 16:25:06 +0300

    input_batch folder added, all the batch jsons and bash scripts are moved there.

--- input_batch/run5-batch.sh
+++ input_batch/run5-batch.sh
@@ -10,4 +10,4 @@
 #SBATCH -p ronnieq
 #SBATCH --mem=1000
 ##SBATCH --time=12:00:00
-python ../newcheb.py --json_rep "input_report.json" --json_task "batch_jsons/input_task_ut_ang_mom_H_run5_var$1.json" > ../batch_jsons_out/out5_$1.txt 2>../batch_jsons_out/err5_$1
+python ../newcheb.py --json_rep "../input_report.json" --json_task "../batch_jsons/input_task_ut_ang_mom_H_run5_var$1.json" > ../batch_jsons_out/out5_$1.txt 2>../batch_jsons_out/err5_$1
