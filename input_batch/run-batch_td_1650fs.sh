5d2b45471319400ba9c5d88ae3023f455b0dfdcd
Irene Mizus <irene.mizus@gmail.com>
2022-10-28 15:56:48 +0300

    input_batch folder added

---
+++ input_batch/run-batch_td_1650fs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+#SBATCH --job-name=try500
+#SBATCH --output=er_%j.txt
+#
+#SBATCH --cpus-per-task=1
+#SBATCH --threads-per-core=1
+##SBATCH --shared
+#SBATCH -A ronnie-account
+#SBATCH -p ronnieq
+#SBATCH --mem=1000
+##SBATCH --time=12:00:00
+python ../newcheb.py --json_rep "input_report_test_dynamics_1650fs.json" --json_task "input_task_ut_ang_mom_H_2lvls_test_dynamics_1650fs.json" > ../batch_jsons_out/out_1650fs.txt 2>../batch_jsons_out/err_1650fs
---
+++ input_batch/run2-batch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+#SBATCH --job-name=try500
+#SBATCH --output=er_%j.txt
+#
+#SBATCH --cpus-per-task=1
+#SBATCH --threads-per-core=1
+##SBATCH --shared
+#SBATCH -A ronnie-account
+#SBATCH -p ronnieq
+#SBATCH --mem=1000
+##SBATCH --time=12:00:00
+python ../newcheb.py --json_rep "input_report.json" --json_task "batch_jsons/input_task_ut_ang_mom_H_run2_var$1.json" > ../batch_jsons_out/out2_$1.txt 2>../batch_jsons_out/err2_$1
