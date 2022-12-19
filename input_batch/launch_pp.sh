08fce662f861c8b8e7deff16bf7f3d3967ce9d2a
Irene Mizus <irene.mizus@gmail.com>
2022-12-19 19:19:08 +0200

    Backup during calculations for 500-600fs

--- input_batch/launch_pp.sh
+++ input_batch/launch_pp.sh
@@ -1,2 +1,2 @@
 echo "Running post_processing.sh"
-sbatch post_processing.sh ../outputs/output_ut_Jx_600/1000-4000fs/fast_ver+960fs/ ../outputs/output_ut_Jx_600/1000-4000fs/fast_ver+960fs/
+sbatch post_processing.sh ../outputs/output_ut_Jx_600/500-600fs/fast_ver+3more/ ../outputs/output_ut_Jx_600/500-600fs/fast_ver+3more/
