0095d3a56f014f84c85b548fee1036e86b343015
Irene Mizus <irene.mizus@gmail.com>
2023-03-03 20:07:09 +0200

    Calculating quantum window till 12000 fs on cluster.

--- input_batch/launch_pp.sh
+++ input_batch/launch_pp.sh
@@ -1,2 +1,2 @@
 echo "Running post_processing.sh"
-sbatch post_processing.sh ../outputs/output_ut_Jx_600/250-9200fs/ ../outputs/output_ut_Jx_600/250-9200fs/
+sbatch post_processing.sh ../outputs/output_ut_Jx_600/250-12000fs/ ../outputs/output_ut_Jx_600/250-12000fs/
