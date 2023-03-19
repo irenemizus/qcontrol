37d5da9b5b36dcd7c0cd7e2d7d0bf43c4545336e
Irene Mizus <irene.mizus@gmail.com>
2023-03-19 13:38:24 +0200

    Running of state-to-state task in progress.

--- generate_jsons.sh
+++ generate_jsons.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python newcheb.py --json_rep "input_batch/input_report.json" --json_task "input_batch/inp_Jx_250-12000fs_25runs.json" --json_create
+python newcheb.py --json_rep "input_batch/input_report.json" --json_task "input_batch/inp_runT_Jx_s2s.json" --json_create
 mv input_task_ut_ang_mom_H_run* batch_jsons
