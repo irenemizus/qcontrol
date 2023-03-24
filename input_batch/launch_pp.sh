24f4c4602b392b7df13685dcb0db67570de8c85c
Irene Mizus <irene.mizus@gmail.com>
2023-03-24 13:03:40 +0300

    Batch files for calculations in time range 250-13400 fs are modified.

--- input_batch/launch_pp.sh
+++ input_batch/launch_pp.sh
@@ -1,2 +1,2 @@
 echo "Running post_processing.sh"
-sbatch post_processing.sh ../outputs/output_ut_Jx_600/250-12000fs/ ../outputs/output_ut_Jx_600/250-12000fs/
+sbatch post_processing.sh ../outputs/output_ut_Jx_600/250-15400fs/ ../outputs/output_ut_Jx_600/250-15400fs/
