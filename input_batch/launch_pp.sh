ecb8208f9fec4f0304a38ebba49106ce97c4bcee
Irene Mizus <irene.mizus@gmail.com>
2023-11-23 17:10:38 +0200

    The latest set of input files for batch runs (still need to fit them to the latest version of the code)

--- input_batch/launch_pp.sh
+++ input_batch/launch_pp.sh
@@ -1,2 +1,2 @@
 echo "Running post_processing.sh"
-sbatch post_processing.sh ../outputs/output_ut_Jx_600/250-15400fs/ ../outputs/output_ut_Jx_600/250-15400fs/
+sbatch post_processing.sh ../outputs/output_ut_Jx_600/13400-26Kfs/ ../outputs/output_ut_Jx_600/13400-26Kfs/
