e4fed703b94dae6157a222e1cf44c03998a217a0
Irene Mizus <40714740+irenemizus@users.noreply.github.com>
2026-05-04 16:07:49 +0300

    Fix Hamiltonian formula for 'z' variant in newcheb.py
    Corrected the formula for the 'z' Hamiltonian variant in the documentation.

--- newcheb.py
+++ newcheb.py
@@ -210,7 +210,7 @@ Options:
         a way of adding the controlling laser field to "BH_model"-type Hamiltonian.
         For all other variants of "hamil_type" variables is a dummy variable.
         Available options:
-        "z" - H = -2 * delta * Jx + 2 * U * Jx**2 + 2 * E(t) * Jz (by default)
+        "z" - H = -2 * delta * Jx + 2 * U * Jz**2 + 2 * E(t) * Jz (by default)
         "x" - H = -2 * delta * Jx * E(t) + 2 * U * Jz + 2 * W * Jz^2
     init_guess
         type of initial guessed laser field envelope used in propagation tasks.
